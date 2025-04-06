7ae9bed88008c60ec1ff81c5c9e0538d32d0d354
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-05 20:56:08 -0500

    Create container to hold stuff

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,11 +1,21 @@
 package;
 
 import flixel.FlxState;
+import flixel.addons.ui.FlxUITabMenu;
 
 class PlayState extends FlxState
 {
+	var container:FlxUITabMenu;
+	final container_tabs = [{name: "Data", label: 'Data'}];
+
 	override public function create():Void
 	{
+		container = new FlxUITabMenu(null, tabs, true);
+
+		container.resize(640, 480);
+		container.screenCenter();
+		add(container);
+
 		super.create();
 	}
 
