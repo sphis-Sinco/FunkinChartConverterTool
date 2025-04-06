09b221ea5d61536a2159ecf534d983fc441f5d56
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-05 21:03:55 -0500

    watermark text

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3,14 +3,22 @@ package;
 import flixel.FlxState;
 import flixel.addons.ui.FlxUI;
 import flixel.addons.ui.FlxUITabMenu;
+import flixel.text.FlxText;
+import sinlib.utilities.Application;
 
 class PlayState extends FlxState
 {
+	var watermark:FlxText = new FlxText(10, 10, 0, '', 16);
+
 	var ui_container:FlxUITabMenu;
-	final ui_container_tabs = [{name: "Data", label: 'Data'}];
+	final ui_container_tabs = [{name: 'Data', label: 'Data'}];
 
 	override public function create():Void
 	{
+		watermark.text = 'FCCT v${Application.VERSION}';
+		watermark.alignment = LEFT;
+		add(watermark);
+
 		ui_container = new FlxUITabMenu(null, ui_container_tabs, true);
 
 		ui_container.resize(640, 480);
@@ -29,10 +37,11 @@ class PlayState extends FlxState
 	{
 		super.update(elapsed);
 	}
+
 	public function addDataTab():Void
 	{
 		var container_group = new FlxUI(null, ui_container);
-		container_group.name = "Data";
+		container_group.name = 'Data';
 
 		ui_container.addGroup(container_group);
 	}
